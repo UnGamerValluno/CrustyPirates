18fa876e784745d7fc84a396f9ae7a7a244e06c8
Jhoan Serrano <jms.co89@gmail.com>
2025-03-05 11:44:54 +0000

    Project Setup

---
+++ Source/CrustyPirates/CrustyPirates.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CrustyPirates.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrustyPirates, "CrustyPirates" );
