3a281e2be77a1e096e4288cda8a16aae1000abf2
Jhoan Serrano <jms.co89@gmail.com>
2025-03-06 12:33:01 +0000

    Input Actions Setup

--- Source/CrustyPirates/PlayerCharacter.cpp
+++ Source/CrustyPirates/PlayerCharacter.cpp
@@ -14,6 +14,16 @@ APlayerCharacter:: APlayerCharacter()
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+
 }
 
 void APlayerCharacter::Tick(float DeltaTime)
@@ -21,3 +31,43 @@ void APlayerCharacter::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// Set up input action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &APlayerCharacter::JumpStarted);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &APlayerCharacter::JumpEnded);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Canceled, this, &APlayerCharacter::JumpEnded);
+		EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Started, this, &APlayerCharacter::Attack);
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+	if (IsAlive && CanMove)
+	{
+		float MoveActionValue = Value.Get<float>();
+		FVector Direction = FVector(1.f, 0.f, 0.f);
+		AddMovementInput(Direction, MoveActionValue);
+	}
+}
+
+void APlayerCharacter::JumpStarted(const FInputActionValue& Value)
+{
+	if (IsAlive && CanMove)
+	{
+		Jump();
+	}
+}
+
+void APlayerCharacter::JumpEnded(const FInputActionValue& Value)
+{
+	StopJumping();
+}
+
+void APlayerCharacter::Attack(const FInputActionValue& Value)
+{
+
+}
--- Source/CrustyPirates/PlayerCharacter.h
+++ Source/CrustyPirates/PlayerCharacter.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
 #include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "InputActionValue.h"
 #include "PaperZDCharacter.h"
 
 #include "PlayerCharacter.generated.h"
@@ -19,7 +24,32 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UCameraComponent* Camera;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* AttackAction;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool IsAlive = true;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool CanMove = true;
+
 	APlayerCharacter();
+
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	void Move(const FInputActionValue& Value);
+	void JumpStarted(const FInputActionValue& Value);
+	void JumpEnded(const FInputActionValue& Value);
+	void Attack(const FInputActionValue& Value);
 };
